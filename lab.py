8793b9c1cd1ba7d566f33f21123e247d38929750
izhaan-raza <izzupro3232@gmail.com>
2024-12-02 22:53:23 +0530

    hello

--- lab.py
+++ lab.py
@@ -1,10 +1,21 @@
-x = int(input("Enter: "))
+# x = int(input("Enter: "))
 
-while(x%10==0):
-    x //=10
-n=0
-while(x>0):
-    n *=10
-    n += x%10
-    x = x//10
-print(n)
+# while(x%10==0):
+#     x //=10
+# n=0
+# while(x>0):
+#     n *=10
+#     n += x%10
+#     x = x//10
+# print(n)
+
+
+
+x = "Hello a b c hello a b b"
+lst = []
+j=0
+for i in range(len(x)):
+    if x[i] == " ":
+        lst.append(x[i:j])
+        j=i
+print(lst)
---
+++ noTorch.py
@@ -0,0 +1,9 @@
+x = [1.0,2.0,3.0]
+y = [12.0, 23.4, 5.4]
+
+bias = 3.3
+weight = 1.2
+
+result = x[0]*weight + x[1]*weight + x[2] + bias
+
+print(result)
